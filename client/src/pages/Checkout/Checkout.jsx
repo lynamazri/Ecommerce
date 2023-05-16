8f96cf030a55aa255f963848e584244c51602bfa
akramch <akram.chaabnia25@gmail.com>
2023-05-16 13:52:27 +0100

    checkout css

--- client/src/pages/Checkout/Checkout.jsx
+++ client/src/pages/Checkout/Checkout.jsx
@@ -23,7 +23,7 @@ function Checkout() {
               <h3>Shipping Address</h3>
               <div className="lower-part">
                 <p>Please enter your billing info</p>
-                <p>Step 1 of 5</p>
+                <p>Step 1 of 3</p>
               </div>
             </div>
             <div className="body">
@@ -32,7 +32,7 @@ function Checkout() {
                 type="text"
                 placeholder="Enter your address"
               />
-              <label className="checkout">
+              <label className="">
                 <input
                   type="checkbox"
                   name="existing-address"
@@ -55,34 +55,39 @@ function Checkout() {
               <h3>Payment method</h3>
               <div className="lower-part">
                 <p>Please enter your payment method</p>
-                <p>Step 2 of 5</p>
+                <p>Step 2 of 3</p>
               </div>
             </div>
-            <div className="body"></div>
-          </div>
-
-          <div className="confirmation"></div>
-          <div className="header">
-            <h3>Confirmation</h3>
-            <div className="lower-part">
-              <p>
-                We are getting to the end. Just few clicks and your order si
-                ready!
-              </p>
-              <p>Step 3 of 5</p>
+            <div className="body">
+              <label className="payment-option">
+                <input type="radio" name="payment-option" value="MagazaCoin" />
+                MagazaCoin
+              </label>
+              <label className="payment-option">
+                <input type="radio" name="payment-option" value="on-delivery" />
+                On Delivery Payment
+              </label>
             </div>
           </div>
 
-          <div className="body">
-            <label>
-              <input
-                type="checkbox"
-                name="existing-address"
-                onChange={handleExistingAddress}
-              />
-              I agree with our terms and conditions and privacy policy.
-            </label>
-            <button className="order-button">Complete order</button>
+          <div className="confirmation">
+            <div className="header">
+              <h3>Confirmation</h3>
+              <div className="lower-part">
+                <p>
+                  We are getting to the end. Just few clicks and your order si
+                  ready!
+                </p>
+                <p>Step 3 of 3</p>
+              </div>
+            </div>
+            <div className="body">
+              <label>
+                <input type="checkbox" name="privacy-policy" />I agree with our
+                terms and conditions and privacy policy.
+              </label>
+              <button className="order-button">Complete order</button>
+            </div>
           </div>
         </div>
 
