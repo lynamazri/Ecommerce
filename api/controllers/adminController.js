1b1755e336038ef1f4ad82cce400fab0446d5aa1
sether1n3 <setherine13@gmail.com>
2023-05-23 22:28:53 +0100

    complaints

--- api/controllers/adminController.js
+++ api/controllers/adminController.js
@@ -29,7 +29,26 @@ const deleteUser = async (req, res) => {
   else res.status(200);
 };
 
+const getUserByUsername = async (req, res) => {
+  const { username } = req.body;
+  const user = await prisma.Users.findUnique({
+    where: {
+      username: username,
+    },
+  });
+  if (!user) res.status(400).send("Unable to find user.");
+  else res.status(200);
+};
+
+const getComplaints = async (req, res) => {
+  const complaints = await prisma.Complaint.findMany();
+  if (!complaints) res.status(400).send("No Complaints found.");
+  else res.status(200).json(complaints);
+};
+
 module.exports = {
   getUsers,
   deleteUser,
+  getUserByUsername,
+  getComplaints,
 };
