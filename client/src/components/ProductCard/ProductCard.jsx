1eace1cf5db1a986c76498e60ce3f616319f8b7f
akramch <akram.chaabnia25@gmail.com>
2023-06-04 20:34:21 +0100

    3awedt wishlist slice/zedt delete prod mn wishlist

--- client/src/components/ProductCard/ProductCard.jsx
+++ client/src/components/ProductCard/ProductCard.jsx
@@ -1,40 +1,50 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { add } from "../../redux/Slices/CartSlice";
-import { addProductToWishlist } from "../../redux/Slices/wishlistSlice";
+import {
+  addProductToWishlist,
+  deleteProductFromWishlist,
+} from "../../redux/Slices/wishlistSlice";
 import { getStars } from "../../utils";
-import { Link } from "react-router-dom";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 import "./ProductCard.css";
+
 function ProductCard({ product, viewMode }) {
   const dispatch = useDispatch();
   const handleAddToCart = () => {
     dispatch(add(product));
   };
-  const user = localStorage.getItem("user")
-    ? JSON.parse(localStorage.getItem("user"))
-    : null;
-  const auth = useSelector((state) => state.auth);
-  const userId = user.userId; // Replace with the actual user ID
-  const products = product.productId; // Replace with the actual product ID
+
+  const userId = useSelector((state) => state.auth.user?.userId);
+  const productId = product.productId;
+  const wishlistItems = useSelector((state) => state.wishlist.items);
+
+  console.log(productId, userId, "productId, userId");
+
+  const isProductInWishlist = wishlistItems.includes(productId);
 
   const handleAddToWishlist = () => {
-    // dispatch(
-    //   addProductToWishlist({ user: auth.user.id, product: product.id })
-    // ).catch((error) => {
-    //   console.error("Error adding product to wishlist:", error);
-    // });
-    dispatch(addProductToWishlist({ userId, products })).catch((error) => {
-      console.error("Error adding product to wishlist:", error);
-    });
+    if (isProductInWishlist) {
+      dispatch(deleteProductFromWishlist({ userId, productId })).catch(
+        (error) => {
+          console.error("Error removing product from wishlist:", error);
+        }
+      );
+    } else {
+      dispatch(addProductToWishlist({ userId, productId })).catch((error) => {
+        console.error("Error adding product to wishlist:", error);
+      });
+    }
   };
 
   return (
     <div className={`product-card ${viewMode === "list" ? "list-view" : ""}`}>
       <div className="product-image">
         <img src={product.images[0].url} alt={product.name} />
-        {product.discount.percentage !== 0 && (
+        {/* {product.discount.percentage !== 0 && (
           <span>-{product.discount.percentage}%</span>
-        )}
+        )} */}
       </div>
       <div className="product-description">
         <Link
@@ -50,7 +60,7 @@ function ProductCard({ product, viewMode }) {
         <div className="product-price">
           {/* {isOnSale ? ( */}
           <>
-            {product.discount.percentage !== 0 ? (
+            {/* {product.discount.percentage !== 0 ? (
               <span>
                 DZD{" "}
                 {product.price -
@@ -61,12 +71,20 @@ function ProductCard({ product, viewMode }) {
             )}
             {product.discount.percentage !== 0 && (
               <span className="old-price">DZD {product.price}</span>
-            )}
+            )} */}
           </>
         </div>
         <button onClick={handleAddToCart}>Add To Cart</button>
         {viewMode === "list" && (
+          // <button className="wishlist-button" onClick={handleAddToWishlist}>
+          //   Add To Wishlist
+          // </button>
           <button className="wishlist-button" onClick={handleAddToWishlist}>
+            {isProductInWishlist ? (
+              <FaHeart style={{ fontSize: "12px", fontWeight: "bold" }} />
+            ) : (
+              <FaRegHeart style={{ fontSize: "12px", fontWeight: "bold" }} />
+            )}
             Add To Wishlist
           </button>
         )}
--- client/src/redux/Slices/wishlistSlice.js
+++ client/src/redux/Slices/wishlistSlice.js
@@ -16,11 +16,14 @@ export const wishlistFetch = createAsyncThunk(
         throw new Error("Authentication token not found.");
       }
 
-      const response = await fetch("/api/wishlist", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
+      const response = await fetch(
+        "http://localhost:3001/api/controllers/profileController/:user/wishlist",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
 
       if (!response.ok) {
         throw new Error("Failed to fetch wishlist items.");
@@ -34,22 +37,94 @@ export const wishlistFetch = createAsyncThunk(
   }
 );
 
-export const addProductToWishlist = createAsyncThunk(
-  "wishlist/addProductToWishlist",
-  async ({ user, product }, { rejectWithValue }) => {
+export const createWishlist = createAsyncThunk(
+  "wishlist/createWishlist",
+  async (userId, { rejectWithValue }) => {
     try {
       const response = await axios.post(
-        `/api/controllers/productController/addProductWish/${user}/${product}`
+        `http://localhost:3001/api/controllers/profileController/createWish`,
+        { userId }
       );
       if (response.status === 200) {
-        return product;
+        return true;
       } else {
-        throw new Error("Unable to add product to wishlist.");
+        throw new Error("Unable to create new wishlist.");
+      }
+    } catch (error) {
+      return rejectWithValue("There was an error creating a new wishlist.");
+    }
+  }
+);
+
+export const addProductToWishlist = createAsyncThunk(
+  "wishlist/addProductToWishlist",
+  async ({ userId, productId }, { rejectWithValue, dispatch }) => {
+    try {
+      const wishlistResponse = await axios.get(
+        `http://localhost:3001/api/controllers/profileController/getWishlist/${userId}`
+      );
+      if (wishlistResponse.status === 200) {
+        const wishlist = wishlistResponse.data;
+
+        // If user doesn't have a wishlist, create one
+        if (!wishlist) {
+          await dispatch(createWishlist());
+        }
+
+        // Add product to the wishlist
+        const response = await axios.post(
+          `http://localhost:3001/api/controllers/productController/addProductWish/${userId}/${productId}`
+        );
+        if (response.status === 200) {
+          return productId;
+        } else {
+          throw new Error("Unable to add product to wishlist.");
+        }
+      } else {
+        throw new Error("Unable to fetch wishlist data.");
       }
     } catch (error) {
       return rejectWithValue(
         "There was an error adding the product to the wishlist."
       );
+    }
+  }
+);
+
+export const deleteProductFromWishlist = createAsyncThunk(
+  "wishlist/deleteProductFromWishlist",
+  async ({ userId, productId }, { rejectWithValue }) => {
+    try {
+      const response = await axios.delete(
+        `http://localhost:3001/api/controllers/productController/deleteProductWish/${userId}/${productId}`
+      );
+      if (response.status === 200) {
+        return productId;
+      } else {
+        throw new Error("Unable to delete product from wishlist.");
+      }
+    } catch (error) {
+      return rejectWithValue(
+        "There was an error deleting the product from the wishlist."
+      );
+    }
+  }
+);
+
+export const getWishlistData = createAsyncThunk(
+  "wishlist/getWishlistData",
+  async ({ userId }, { rejectWithValue }) => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3001/api/controllers/profileController/getWishlist/${userId}`
+      );
+      if (response.status === 200) {
+        return response.data;
+      } else {
+        throw new Error("Unable to fetch wishlist data.");
+      }
+    } catch (error) {
+      return rejectWithValue("There was an error fetching the wishlist data.");
     }
   }
 );
@@ -75,16 +150,37 @@ const wishlistSlice = createSlice({
         state.error = action.payload;
         state.items = [];
       })
+      .addCase(createWishlist.fulfilled, (state) => {
+        state.items = [];
+      })
+      .addCase(createWishlist.rejected, (state, action) => {
+        state.error = action.payload;
+      })
       .addCase(addProductToWishlist.fulfilled, (state, action) => {
         state.items.push(action.payload);
       })
       .addCase(addProductToWishlist.rejected, (state, action) => {
+        state.error = action.payload;
+      })
+      .addCase(deleteProductFromWishlist.fulfilled, (state, action) => {
+        state.items = state.items.filter(
+          (product) => product !== action.payload
+        );
+      })
+      .addCase(deleteProductFromWishlist.rejected, (state, action) => {
+        state.error = action.payload;
+      })
+      .addCase(getWishlistData.fulfilled, (state, action) => {
+        state.items = action.payload.products;
+      })
+      .addCase(getWishlistData.rejected, (state, action) => {
         state.error = action.payload;
       });
   },
 });
 
 // Export the additional actions
-export const { wishlistAdd } = wishlistSlice.actions;
+export const { wishlistAdd, wishlistRemove, wishlistClear } =
+  wishlistSlice.actions;
 
 export default wishlistSlice.reducer;
