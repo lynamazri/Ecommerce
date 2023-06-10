768980471a6ba89c4d5b17909676c163f063f6f1
akramch <akram.chaabnia25@gmail.com>
2023-06-10 05:34:41 +0100

    nahit erreur

--- client/src/components/AdminDashboard/AdminSettings.jsx
+++ client/src/components/AdminDashboard/AdminSettings.jsx
@@ -1,21 +1,23 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
 
 function AdminSettings() {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const [formData, setFormData] = useState({
+  const [adminInfo, setAdminInfo] = useState({
     firstName: "",
     lastName: "",
     newUsername: "",
     bankAccountNumber: "",
+  });
+  const [passwordInfo, setPasswordInfo] = useState({
     oldPass: "",
     newPass: "",
     confirmNewPass: "",
   });
-  const [errorMessage, setErrorMessage] = useState("");
-  const [confirmationMessage, setConfirmationMessage] = useState("");
+  const [adminInfoErrorMessage, setAdminInfoErrorMessage] = useState("");
+  const [passwordErrorMessage, setPasswordErrorMessage] = useState("");
+  const [adminInfoConfirmationMessage, setAdminInfoConfirmationMessage] =
+    useState("");
+  const [passwordConfirmationMessage, setPasswordConfirmationMessage] =
+    useState("");
   const [passInputType, setPassInputType] = useState({
     oldPass: "password",
     newPass: "password",
@@ -26,45 +28,62 @@ function AdminSettings() {
     newPass: false,
     confirmNewPass: false,
   });
-  const [updatePassword] = usePatchPasswordMutation();
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+  const handleAdminInfoChange = (e) => {
+    setAdminInfo({ ...adminInfo, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
+  const handlePasswordChange = (e) => {
+    setPasswordInfo({ ...passwordInfo, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmitAdminInfo = (e) => {
     e.preventDefault();
 
-    // Shop Name validation: Minimum 2 characters
-    if (formData.firstName.length < 2) {
-      setErrorMessage("First Name must be at least 2 characters long.");
-      return false;
+    // Admin info validation
+    if (adminInfo.firstName.length < 2) {
+      setAdminInfoErrorMessage(
+        "First Name must be at least 2 characters long."
+      );
+      return;
     }
 
-    // Last Name validation: Minimum 2 characters
-    if (formData.lastName.length < 2) {
-      setErrorMessage("Last Name must be at least 2 characters long.");
-      return false;
+    if (adminInfo.lastName.length < 2) {
+      setAdminInfoErrorMessage("Last Name must be at least 2 characters long.");
+      return;
     }
 
-    // Username validation: Minimum 2 characters
-    if (formData.newUsername.length < 2) {
-      setErrorMessage("Username must be at least 2 characters long.");
-      return false;
+    if (adminInfo.newUsername.length < 2) {
+      setAdminInfoErrorMessage("Username must be at least 2 characters long.");
+      return;
     }
 
-    // Bank Account Number validation: Minimum 2 characters
-    if (formData.bankAccountNumber.length < 2) {
-      setErrorMessage(
+    if (adminInfo.bankAccountNumber.length < 2) {
+      setAdminInfoErrorMessage(
         "Bank Account Number must be at least 2 characters long."
       );
-      return false;
+      return;
     }
 
-    // Add your logic here to handle form submission and updating admin's information
+    // Handle admin info submission
+    // ...
 
-    // Example logic to display confirmation message
-    setConfirmationMessage("Changes saved successfully.");
+    setAdminInfoConfirmationMessage("Changes saved successfully.");
+  };
+
+  const handleSubmitPassword = (e) => {
+    e.preventDefault();
+
+    // Password validation
+    if (passwordInfo.newPass !== passwordInfo.confirmNewPass) {
+      setPasswordErrorMessage("Passwords do not match.");
+      return;
+    }
+
+    // Handle password submission
+    // ...
+
+    setPasswordConfirmationMessage("Password updated successfully.");
   };
 
   const togglePasswordInputType = (name) => {
@@ -78,147 +97,99 @@ function AdminSettings() {
     }));
   };
 
-  const validateForm = () => {
-    if (formData.newPass !== formData.confirmNewPass) {
-      setErrorMessage("Passwords do not match.");
-      return false;
-    }
-    return true;
-  };
-
-  useEffect(() => {
-    if (confirmationMessage || errorMessage) {
-      const timer = setTimeout(() => {
-        setConfirmationMessage("");
-        setErrorMessage("");
-      }, 5000);
-      return () => clearTimeout(timer);
-    }
-  }, [confirmationMessage, errorMessage]);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (!validateForm()) {
-      return;
-    }
-
-    // Handle form submission and updating admin's information
-
-    // Example logic to display confirmation message
-    setConfirmationMessage("Changes saved successfully.");
-
-    // Update password
-    updatePassword({
-      curPassword: formData.oldPass,
-      newPassword: formData.newPass,
-      userId: formData.userId,
-    })
-      .unwrap() // Extract the response data
-      .then(() => {
-        // Handle successful update
-        setConfirmationMessage("Password updated successfully.");
-        setFormData({
-          ...formData,
-          oldPass: "",
-          newPass: "",
-          confirmNewPass: "",
-        });
-      })
-      .catch(() => {
-        // Handle error
-        setErrorMessage("Error updating password.");
-      });
-  };
-
   return (
     <div className="admin-settings-page admin--page dashboard--page">
       <div className="header">
         <h3>
-          Hello, {formData.firstName} {formData.lastName}
+          Hello, {adminInfo.firstName} {adminInfo.lastName}
         </h3>
         <p>Update your personal information by changing the inputs below.</p>
       </div>
       <div className="main">
         <h3 className="upper">Settings</h3>
+
+        {/* Admin Info */}
         <div className="content">
           <div className="header">
-            <h4>Edit Shop Info</h4>
-            <p>Fill in the inputs below to change your password</p>
+            <h4>Edit Admin Info</h4>
+            <p>Fill in the inputs below to update your admin information.</p>
           </div>
 
-          <form className="admin-info-form" onSubmit={handleSubmit}>
-                    <div className="my-profile-inputs">
-                      <div className="input-container">
-            <label htmlFor="firstName">First Name</label>
-            <input
-              type="text"
-              name="firstName"
-              id="firstName"
-              placeholder="First Name"
-              required
-              onChange={handleChange}
-              value={formData.firstName}
-            />
-          </div>
-          <div className="input-container">
-            <label htmlFor="lastName">Last Name</label>
-            <input
-              type="text"
-              name="lastName"
-              id="lastName"
-              placeholder="Last Name"
-              required
-              onChange={handleChange}
-              value={formData.lastName}
-            />
-          </div>
-          <div className="input-container">
-            <label htmlFor="newUsername">Username</label>
-            <input
-              type="text"
-              name="newUsername"
-              id="newUsername"
-              placeholder="Username"
-              required
-              onChange={handleChange}
-              value={formData.newUsername}
-            />
-          </div>
-          <div className="input-container">
-            <label htmlFor="bankAccountNumber">Bank Account Number</label>
-            <input
-              type="text"
-              name="bankAccountNumber"
-              id="bankAccountNumber"
-              placeholder="Bank Account Number"
-              required
-              onChange={handleChange}
-              value={formData.bankAccountNumber}
-            />
-          </div>
-        </div>
-
-        {errorMessage && <p className="error-message">{errorMessage}</p>}
-        {confirmationMessage && (
-          <p className="confirmation-message">{confirmationMessage}</p>
-        )}
-
-        <div className="input-container">
-          <button type="submit" value="Submit">
-            Save Changes
-          </button>
-        </div>
-                  </form>
-
-        </div>
-
-          <div className="password">
-            <div className="header">
-              <h4>Security</h4>
-              <p>Fill in the inputs below to change your password</p>
+          <form className="admin-info-form" onSubmit={handleSubmitAdminInfo}>
+            <div className="my-profile-inputs">
+              <div className="input-container">
+                <label htmlFor="firstName">First Name</label>
+                <input
+                  type="text"
+                  name="firstName"
+                  id="firstName"
+                  placeholder="First Name"
+                  required
+                  onChange={handleAdminInfoChange}
+                  value={adminInfo.firstName}
+                />
+              </div>
+              <div className="input-container">
+                <label htmlFor="lastName">Last Name</label>
+                <input
+                  type="text"
+                  name="lastName"
+                  id="lastName"
+                  placeholder="Last Name"
+                  required
+                  onChange={handleAdminInfoChange}
+                  value={adminInfo.lastName}
+                />
+              </div>
+              <div className="input-container">
+                <label htmlFor="newUsername">Username</label>
+                <input
+                  type="text"
+                  name="newUsername"
+                  id="newUsername"
+                  placeholder="Username"
+                  required
+                  onChange={handleAdminInfoChange}
+                  value={adminInfo.newUsername}
+                />
+              </div>
+              <div className="input-container">
+                <label htmlFor="bankAccountNumber">Bank Account Number</label>
+                <input
+                  type="text"
+                  name="bankAccountNumber"
+                  id="bankAccountNumber"
+                  placeholder="Bank Account Number"
+                  required
+                  onChange={handleAdminInfoChange}
+                  value={adminInfo.bankAccountNumber}
+                />
+              </div>
             </div>
-            <div className="security-inputs">
+
+            {adminInfoErrorMessage && (
+              <p className="error-message">{adminInfoErrorMessage}</p>
+            )}
+            {adminInfoConfirmationMessage && (
+              <p className="confirmation-message">
+                {adminInfoConfirmationMessage}
+              </p>
+            )}
+
+            <div className="input-container">
+              <button type="submit">Save Changes</button>
+            </div>
+          </form>
+        </div>
+
+        {/* Password */}
+        <div className="password">
+          <div className="header">
+            <h4>Security</h4>
+            <p>Fill in the inputs below to change your password.</p>
+          </div>
+          <div className="security-inputs">
+            <form onSubmit={handleSubmitPassword}>
               <div className="input-container">
                 <label htmlFor="oldPass">Current Password</label>
                 <div className="input-password">
@@ -228,10 +199,10 @@ function AdminSettings() {
                     id="oldPass"
                     placeholder="Current Password"
                     required
-                    onChange={handleChange}
-                    value={formData.oldPass}
+                    onChange={handlePasswordChange}
+                    value={passwordInfo.oldPass}
                   />
-                  {formData.oldPass && (
+                  {passwordInfo.oldPass && (
                     <button
                       className="show-password"
                       onClick={(e) => {
@@ -239,11 +210,7 @@ function AdminSettings() {
                         togglePasswordInputType("oldPass");
                       }}
                     >
-                      {showPass.oldPass ? (
-                        <AiFillEye />
-                      ) : (
-                        <AiFillEyeInvisible />
-                      )}
+                      {showPass.oldPass ? "Hide" : "Show"}
                     </button>
                   )}
                 </div>
@@ -257,10 +224,10 @@ function AdminSettings() {
                     id="newPass"
                     placeholder="New Password"
                     required
-                    onChange={handleChange}
-                    value={formData.newPass}
+                    onChange={handlePasswordChange}
+                    value={passwordInfo.newPass}
                   />
-                  {formData.newPass && (
+                  {passwordInfo.newPass && (
                     <button
                       className="show-password"
                       onClick={(e) => {
@@ -268,11 +235,7 @@ function AdminSettings() {
                         togglePasswordInputType("newPass");
                       }}
                     >
-                      {showPass.newPass ? (
-                        <AiFillEye />
-                      ) : (
-                        <AiFillEyeInvisible />
-                      )}
+                      {showPass.newPass ? "Hide" : "Show"}
                     </button>
                   )}
                 </div>
@@ -286,10 +249,10 @@ function AdminSettings() {
                     id="confirmNewPass"
                     placeholder="Confirm New Password"
                     required
-                    onChange={handleChange}
-                    value={formData.confirmNewPass}
+                    onChange={handlePasswordChange}
+                    value={passwordInfo.confirmNewPass}
                   />
-                  {formData.confirmNewPass && (
+                  {passwordInfo.confirmNewPass && (
                     <button
                       className="show-password"
                       onClick={(e) => {
@@ -297,25 +260,25 @@ function AdminSettings() {
                         togglePasswordInputType("confirmNewPass");
                       }}
                     >
-                      {showPass.confirmNewPass ? (
-                        <AiFillEye />
-                      ) : (
-                        <AiFillEyeInvisible />
-                      )}
+                      {showPass.confirmNewPass ? "Hide" : "Show"}
                     </button>
                   )}
                 </div>
               </div>
-            </div>
-            {errorMessage && <p className="error-message">{errorMessage}</p>}
-            {confirmationMessage && (
-              <p className="confirmation-message">{confirmationMessage}</p>
-            )}
-            <div className="button-container">
-              <button type="submit" value="Submit">
-                Update Password
-              </button>
-            </div>
+
+              {passwordErrorMessage && (
+                <p className="error-message">{passwordErrorMessage}</p>
+              )}
+              {passwordConfirmationMessage && (
+                <p className="confirmation-message">
+                  {passwordConfirmationMessage}
+                </p>
+              )}
+
+              <div className="input-container">
+                <button type="submit">Save Changes</button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
